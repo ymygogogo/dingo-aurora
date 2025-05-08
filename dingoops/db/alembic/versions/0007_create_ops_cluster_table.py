99c392d52b1aa8d18c9d3f0549a48ee7656e1fb9
zhangjing2 <zhangjing2@zetyun.com>
2025-05-08 20:52:41 +0800

    [feature][cluster]add instance and node alembix script

--- dingoops/db/alembic/versions/0007_create_ops_cluster_table.py
+++ dingoops/db/alembic/versions/0007_create_ops_cluster_table.py
@@ -14,8 +14,8 @@ import sqlalchemy as sa
 from datetime import datetime
 
 # revision identifiers, used by Alembic.
-revision: str = '0006'
-down_revision: Union[str, None] = '0005'
+revision: str = '0007'
+down_revision: Union[str, None] = '0006'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
